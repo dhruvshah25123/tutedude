dc832645360421cd2a7fb120c0ad3cf298dbb243
dhruvshah25123 <dhruvshah6804@gmail.com>
2025-05-19 09:55:48 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,14 @@
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter another number: "))
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+print("Addition: ", addition)
+print("Subtraction: ", subtraction)
+print("Multiplication: ", multiplication)
+print("Division: ", division)
+
+name =input("Enter your First name: ")
+surname=input("Enter your last name: ")
+print(f"Welcome {name} {surname} to the Python Programme")
